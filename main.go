a3cc17f635d96f8341ecb00593e7c4e8190adffa
Jeff Prestes <jeffprestes@gmail.com>
2018-09-16 21:34:19 -0300

    add function hash to filter logs

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -60,14 +61,34 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// NOTE: keccak256("LogFill(address,address,address,address,address,uint256,uint256,uint256,uint256,bytes32,bytes32)")
+	logFillEvent := common.HexToHash("0d0b9391970d9a25552f37d436d2aae2925e2bfe1b2a923754bada030c498cb3")
+
 	for _, vLog := range logs {
-		log.Printf("Log: %d\n", vLog.Index)
-		fillEvent := LogFill{}
-		err := contractAbi.Unpack(&fillEvent, "LogFill", vLog.Data)
-		if err != nil {
-			fmt.Println(" ")
-			log.Println(err)
-			fmt.Println(" ")
+		fmt.Printf("Log Index: %d\n", vLog.Index)
+		if vLog.Topics[0].Hex() == logFillEvent.Hex() {
+			fillEvent := LogFill{}
+			err := contractAbi.Unpack(&fillEvent, "LogFill", vLog.Data)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fillEvent.Maker = common.HexToAddress(vLog.Topics[1].Hex())
+			fillEvent.FeeRecipient = common.HexToAddress(vLog.Topics[2].Hex())
+			fillEvent.Tokens = vLog.Topics[3]
+
+			fmt.Printf("Maker: %s\n", fillEvent.Maker.Hex())
+			fmt.Printf("Taker: %s\n", fillEvent.Taker.Hex())
+			fmt.Printf("Maker Token: %s\n", fillEvent.MakerToken.Hex())
+			fmt.Printf("Taker Token: %s\n", fillEvent.TakerToken.Hex())
+			fmt.Printf("Fee Recipient: %s\n", fillEvent.FeeRecipient.Hex())
+			fmt.Printf("Filled Maker Token Amount: %s\n", fillEvent.FilledMakerTokenAmount.String())
+			fmt.Printf("Filled Taker Token Amount: %s\n", fillEvent.FilledTakerTokenAmount.String())
+			fmt.Printf("Paid Maker Fee: %s\n", fillEvent.PaidMakerFee.String())
+			fmt.Printf("Paid Taker Fee: %s\n", fillEvent.PaidTakerFee.String())
+			fmt.Printf("Tokens: %s\n", hexutil.Encode(fillEvent.Tokens[:]))
+			fmt.Printf("Order Hash: %s\n", hexutil.Encode(fillEvent.OrderHash[:]))
 		}
+		fmt.Printf("\n\n")
 	}
 }
